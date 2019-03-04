e11edc20b4c69990c449955207e18c71c7ef662a
antonioalmeida <theantonioalmeida@gmail.com>
2019-03-04 16:07:29 +0000

    Implement dna sequence validation and symbol frequency

--- project1/BioSeq.py
+++ project1/BioSeq.py
@@ -1,10 +1,27 @@
+import abc
 from SequenceType import SequenceType
 
-class BioSeq:
+class BioSeq(abc.ABC):
 
+    # Constructor 
     def __init__(self, seq, seq_type):
         self.seq = seq
         self.seq_type = seq_type
 
+    @abc.abstractmethod
+    def is_valid(self):
+        """Checks if the sequence is valid, depending on its type."""
+
+    def symbol_frequency(self):
+        """Frequency of Symbols"""
+        dic = {}
+        for i in self.seq:
+            if(i not in dic):
+                dic[i] = 1
+            else:
+                dic[i] += 1
+        return dic
+
     def __str__(self):
         return self.seq + ' - ' + str(self.seq_type)
+
--- project1/DNASeq.py
+++ project1/DNASeq.py
@@ -2,6 +2,11 @@ from BioSeq import BioSeq
 from SequenceType import SequenceType
 
 class DNASeq(BioSeq):
+
+    nucleotides_dic = set(['A', 'T', 'C', 'G'])
     
     def __init__(self, seq):
         super().__init__(seq, SequenceType.DNA)
+
+    def is_valid(self):
+        return all(i in DNASeq.nucleotides_dic for i in self.seq)
