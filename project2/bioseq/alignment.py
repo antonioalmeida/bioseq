f9280a503b79d90b2090c19f3e1c9470b219ec8b
antonioalmeida <theantonioalmeida@gmail.com>
2019-04-13 17:30:52 +0100

    implement base recover alignment

--- project2/bioseq/alignment.py
+++ project2/bioseq/alignment.py
@@ -1,5 +1,9 @@
 import abc
 
+DIAGONAL = 1
+VERTICAL = 2
+HORIZONTAL = 3
+
 class Alignment():
 
     sm = []
@@ -7,7 +11,7 @@ class Alignment():
     def __init__(self, sm_file='bioseq/res/blosum62.mat'):
         self.sm = self.__read_substitution_matrix_file(sm_file)
 
-    def needleman_wunsch(self, seq1, seq2, sm, g):
+    def needleman_wunsch(self, seq1, seq2, g):
         m = len(seq1)
         n = len(seq2)
         
@@ -27,13 +31,33 @@ class Alignment():
         # apply the recurrence to fill the matrices
         for i in range(1, m+1):
             for j in range(1, n+1):
-                s1 = score[i-1][j-1] + self.__score_col_alignment(seq1[i-1], seq2[j-1], sm, g)
+                s1 = score[i-1][j-1] + self.__score_col_alignment(seq1[i-1], seq2[j-1], g)
                 s2 = score[i-1][j] + g
                 s3 = score[i][j-1] + g
                 score[i].append(max(s1,s2,s3))
                 trace[i].append(self.__max3t(s1,s2,s3))
         
         return (score, trace)
+
+    def recover_align(self, trace, seq1, seq2):
+        res = ["",""]
+        (i,j) = (len(seq1),len(seq2))
+        
+        while i>0 or j>0:
+            if trace[j][i] is DIAGONAL:
+                res[0] += seq1[i-1]
+                res[1] += seq2[j-1]
+                i -= 1; j -= 1
+            elif trace[j][i] is VERTICAL:
+                res[0] += '_'
+                res[1] += seq2[j-1]
+                j -= 1
+            elif trace[j][i] is HORIZONTAL:
+                res[0] += seq1[i-1]
+                res[1] += '_'
+                i -= 1
+        
+        return [s[::-1] for s in res]
 
     def __max3t(self, v1,v2,v3):
         if v1 > v2:
@@ -43,10 +67,9 @@ class Alignment():
 
     # Provides the score of a column alignment, i.e., between characters c1 and c2
     # Assume a constant gap penalty g and a substitution matrix sm
-    def __score_col_alignment(self, c1, c2, sm, g):
-        return g if c1 == '-' or c2 == '-' else sm[c1+c2]
+    def __score_col_alignment(self, c1, c2, g):
+        return g if c1 == '-' or c2 == '-' else self.sm[c1+c2]
 
-    
     def __read_substitution_matrix_file(self, filename):
         
         with open(filename) as f:
