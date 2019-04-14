606171e44f4b197d1e2e5e5a2b91dd4d8c8c4f89
antonioalmeida <theantonioalmeida@gmail.com>
2019-04-15 00:12:55 +0100

    refactor alignment file

--- project2/bioseq/alignment.py
+++ project2/bioseq/alignment.py
@@ -25,7 +25,6 @@ class Alignment():
 
         debug_2 = not (len(seq1) > 15 or len(seq2) > 15)
         (s,t) = self.global_align_multiple_solutions(seq1, seq2, g, debug_2)
-        
         alignments = self.recover_global_align_multiple_solutions(seq1, seq2, t, g, debug)
 
         if debug:
@@ -47,8 +46,7 @@ class Alignment():
             print()
 
         debug_2 = not (len(seq1) > 15 or len(seq2) > 15)
-        (s,t,max_score) = self.local_align_multiple_solutions(seq1, seq2, g, debug_2)
-        
+        (s,t,_) = self.local_align_multiple_solutions(seq1, seq2, g, debug_2)
         alignments = self.recover_local_align_multiple_solutions(seq1, seq2, s, t, debug)
 
         if debug:
@@ -63,40 +61,29 @@ class Alignment():
         
 
     def global_align_multiple_solutions(self, seq1, seq2, g, debug=False):
-        m = len(seq1)
-        n = len(seq2)
-        
-        score = [[0]]
-        trace = [[0]]
+        m = len(seq1); n = len(seq2)
+        S = [[0]]; T = [[0]]
         
         # initialize gaps in rows
-        score[0] = [g * j for j in range(0, n+1)]
-        trace[0] = [[3] for _ in range(0, n+1)]
+        S[0] = [g * j for j in range(0, n+1)]
+        T[0] = [[3] for _ in range(0, n+1)]
         
         # initialize gaps in cols
         for i in range(1, m+1):
-            score.append([g*i])
-            trace.append([[2]])
+            S.append([g*i])
+            T.append([[2]])
 
         # apply the recurrence to fill the matrices
         for i in range(1, m+1):
             for j in range(1, n+1):
-                s1 = score[i-1][j-1] + self.__score_col_alignment(seq1[i-1], seq2[j-1], g)
-                s2 = score[i-1][j] + g
-                s3 = score[i][j-1] + g
-                score[i].append(max(s1,s2,s3))
-                trace[i].append(self.__max_indices(s1,s2,s3))
+                s1 = S[i-1][j-1] + self.__score_col_alignment(seq1[i-1], seq2[j-1], g)
+                s2 = S[i-1][j] + g
+                s3 = S[i][j-1] + g
+                S[i].append(max(s1,s2,s3))
+                T[i].append(self.__max_indices(s1,s2,s3))
         
-        if debug:
-            print()
-            print('> Score')
-            utils.pretty_print_matrix(score)
-
-            print()
-            print('> Trace')
-            utils.pretty_print_matrix(trace)
-        
-        return (score, trace)
+        if debug: utils.print_tc(S, T)
+        return (S, T)
 
     def recover_global_align_multiple_solutions(self, seq1, seq2, trace, g, debug=False):
         final = []
@@ -108,62 +95,41 @@ class Alignment():
                 final.append([al_1, al_2])
             else:
                 for move in trace[i][j]:
-                    next_al_1 = ""; next_al_2 = ""
-                    next_i = i; next_j = j
-                    if move is DIAGONAL:
-                        next_al_1 = seq1[i-1] + al_1
-                        next_al_2 = seq2[j-1] + al_2
-                        next_i = i-1; next_j = j-1
-                    elif move is HORIZONTAL:
-                        next_al_1 = '_' + al_1
-                        next_al_2 = seq2[j-1] + al_2
-                        next_j = j-1
-                    elif move is VERTICAL:
-                        next_al_1 = seq1[i-1] + al_1
-                        next_al_2 = '_' + al_2
-                        next_i = i-1
-                    new_alignment = [next_al_1, next_al_2, next_i, next_j]
-                    alignments.append(new_alignment)
-
+                    alignments.append(self._recover_step(move, seq1, seq2, al_1, al_2, i, j))
         return final
 
     def local_align_multiple_solutions(self, seq1, seq2, g, debug=False):
+        m = len(seq1); n = len(seq2)
         """Local alignment"""
-        score = [[0]]
-        trace = [[[0]]]
-        maxscore = 0
+        S = [[0]]; T = [[[0]]]
+        max_s = 0
+
         # first row filled with zero
-        for j in range(1, len(seq2)+1):
-            score[0].append(0)
-            trace[0].append([0])
+        for j in range(1, n+1):
+            S[0].append(0)
+            T[0].append([0])
         # first column filled with zero
-        for i in range(1, len(seq1)+1):
-            score.append([0])
-            trace.append([[0]])
-        for i in range(0, len(seq1)):
-            for j in range(len(seq2)):
-                s1 = score[i][j] + self.__score_col_alignment(seq1[i], seq2[j], g)
-                s2 = score[i][j+1] + g
-                s3 = score[i+1][j] + g
+        for i in range(1, m+1):
+            S.append([0])
+            T.append([[0]])
+
+        for i in range(0, m):
+            for j in range(n):
+                s1 = S[i][j] + self.__score_col_alignment(seq1[i], seq2[j], g)
+                s2 = S[i][j+1] + g
+                s3 = S[i+1][j] + g
                 b = max(s1, s2, s3)
                 if b <= 0:
-                    score[i+1].append(0)
-                    trace[i+1].append([0])
+                    S[i+1].append(0)
+                    T[i+1].append([0])
                 else:
-                    score[i+1].append(b)
-                    trace[i+1].append(self.__max_indices(s1, s2, s3))
-                    if b > maxscore:
-                        maxscore = b
+                    S[i+1].append(b)
+                    T[i+1].append(self.__max_indices(s1, s2, s3))
+                    if b > max_s:
+                        max_s = b
 
-        if debug:
-            print()
-            print('> Score')
-            utils.pretty_print_matrix(score)
-
-            print()
-            print('> Trace')
-            utils.pretty_print_matrix(trace)
-        return (score, trace, maxscore) 
+        if debug: utils.print_tc(S, T)
+        return (S, T, max_s) 
 
     def recover_local_align_multiple_solutions(self, seq1, seq2, S, T, debug=False):
         final = []
@@ -177,31 +143,11 @@ class Alignment():
 
         while alignments:
             [al_1, al_2, i, j] = alignments.pop()
-            if T[i][j] == 0:
-                final.append([al_1, al_2])
-            else: 
-                for move in T[i][j]:
-                    next_al_1 = ""; next_al_2 = ""
-                    next_i = i; next_j = j
-                    if move is TERMINATION:
-                        final.append([al_1, al_2])
-                    else:
-                        next_al_1 = ""; next_al_2 = ""
-                        next_i = i; next_j = j
-                        if move is DIAGONAL:
-                            next_al_1 = seq1[i-1] + al_1
-                            next_al_2 = seq2[j-1] + al_2
-                            next_i = i-1; next_j = j-1
-                        elif move == HORIZONTAL:
-                            next_al_1 = '_' + al_1
-                            next_al_2 = seq2[j-1] + al_2
-                            next_j = j-1
-                        elif move is VERTICAL:
-                            next_al_1 = seq1[i-1] + al_1
-                            next_al_2 = '_' + al_2
-                            next_i = i-1
-                        new_alignment = [next_al_1, next_al_2, next_i, next_j]
-                        alignments.append(new_alignment)
+            for move in T[i][j]:
+                if move == TERMINATION:
+                    final.append([al_1, al_2])
+                else:
+                    alignments.append(self._recover_step(move, seq1, seq2, al_1, al_2, i, j))
         return final 
 
     def compare_pairwise_global_align(self, seqs, g):
@@ -213,6 +159,18 @@ class Alignment():
         scores = [ [self.__get_local_alignment_max_score(seq1, seq2, g) for seq2 in seqs] for seq1 in seqs ]
         utils.pretty_print_with_header(scores, seqs)
         return scores
+
+    def _recover_step(self, move, s1, s2, l, r, i, j):
+        if move == DIAGONAL:
+            l = s1[i-1] + l; r = s2[j-1] + r
+            i -= 1; j-= 1
+        elif move == HORIZONTAL:
+            l = '_' + l; r = s2[j-1] + r
+            j-= 1
+        elif move == VERTICAL:
+            l = s1[i-1] + l; r = '_' + r
+            i-= 1
+        return (l,r,i,j)
 
     def __max3t(self, v1,v2,v3):
         if v1 > v2:
