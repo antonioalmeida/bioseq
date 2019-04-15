5a2631468f9cda3101cde1d08aafa1ccdf297357
antonioalmeida <theantonioalmeida@gmail.com>
2019-04-15 02:39:20 +0100

    implement new utils functions

--- project2/utils.py
+++ project2/utils.py
@@ -1,19 +1,15 @@
+def pretty_print_with_header(m, seqs):
+    matrix = []
+    header = [' ']
+    header += (seqs)
+    matrix.append(header)
 
-def pretty_print(seq1, seq2, m):
-    
-    for line in m:
-        pretty_row(line)
+    for i,line in enumerate(m):
+        n_line = [seqs[i][:3]]
+        n_line += line
+        matrix.append(n_line)
 
-def pretty_header(seq):
-    print('    ', end="")
-    for i in seq:
-        print(i + ' | ', end="")
-    print()
-
-def pretty_row(row):
-    for i in row:
-        print(str(i) + ' | ', end="")
-    print()
+    pretty_print_matrix(matrix)
 
 def pretty_print_matrix(m):
     s = [[str(e) for e in row] for row in m]
@@ -21,3 +17,12 @@ def pretty_print_matrix(m):
     fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
     table = [fmt.format(*row) for row in s]
     print('\n'.join(table))
+
+def print_tc(score, trace):
+    print()
+    print('> Score')
+    pretty_print_matrix(score)
+
+    print()
+    print('> Trace')
+    pretty_print_matrix(trace)
