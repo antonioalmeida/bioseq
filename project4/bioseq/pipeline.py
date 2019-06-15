c028e2e486c434b296c589707c012176be0de5c8
antonioalmeida <theantonioalmeida@gmail.com>
2019-06-15 16:49:56 +0100

    add pipeline constructor test

--- project4/bioseq/pipeline.py
+++ project4/bioseq/pipeline.py
@@ -2,11 +2,11 @@ from bioseq.fasta import read_fasta_file
 
 class Pipeline():
 
-    description = ""
+    desc = ""
     seq = ""
 
     def __init__(self, filename, seq_type='dna'):
-        self.description, self.seq = list(read_fasta_file(filename, seq_type).items())[0]
+        self.desc, self.seq = list(read_fasta_file(filename, seq_type).items())[0]
 
 
         
