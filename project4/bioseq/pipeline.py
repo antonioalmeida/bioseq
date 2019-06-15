64779384a640035e0661653e1a99f77cd9f5aa7e
antonioalmeida <theantonioalmeida@gmail.com>
2019-06-15 16:42:05 +0100

    implement single seq extraction from fasta file

--- project4/bioseq/pipeline.py
+++ project4/bioseq/pipeline.py
@@ -2,9 +2,12 @@ from bioseq.fasta import read_fasta_file
 
 class Pipeline():
 
+    description = ""
     seq = ""
 
     def __init__(self, filename, seq_type='dna'):
-        self.seq = read_fasta_file(filename, seq_type)
+        self.description, self.seq = list(read_fasta_file(filename, seq_type).items())[0]
+
+
         
 
