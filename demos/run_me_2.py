b3f090adc34da4bcaceecd6e4180d700fea196a4
antonioalmeida <theantonioalmeida@gmail.com>
2019-08-01 00:18:02 +0100

    further cleanup

--- demos/run_me_2.py
+++ demos/run_me_2.py
@@ -1,89 +1,73 @@
-import bioseq
+import bioseq 
+import itertools
 
 def press_enter():
     print()
     input('Press ENTER to continue...')
-    for i in range(0,10): print()
+    for i in range(0,50): print()
 
-valid_dna = 'ATGAAATTATGAATGAGCCTCAGCTGAAGCATCGCGCATCAGACTACGCTCAGACTCAGACTCAGCATTATAGTGAATGTTAATAAATAAAATAA'
+print('Welcome to the bioseq v2 package demo.')
 
-print('Welcome to the bioseq package demo.')
-print('We\'ll be using the sample DNA sequence - ' + valid_dna)
+print('> C1')
+print('We\'ll now be calculating global and local alignments from protein_sequences.fas file')
+
+# Test C1
+al = bioseq.alignment() # this uses bosum62 by default
+fasta_dic = bioseq.read_fasta_file('res/protein_sequences.fas', 'aminoacid') 
+pairs = list(itertools.combinations(fasta_dic.keys(), 2))
+alignments = []
+
+print('Calculating alignments...')
+for (key1, key2) in pairs:
+    seq1 = fasta_dic[key1]; seq2 = fasta_dic[key2]
+    n_global_alignments = len(al.run_global_align_multiple_solutions(seq1, seq2, -3))
+
+    (s,t, _) = al.local_align_multiple_solutions(seq1, seq2, -3)
+    n_local_alignments = len(al.recover_local_align_multiple_solutions(seq1, seq2,s,t))
+
+    alignments.append([key1, key2, n_global_alignments, n_local_alignments])
+
+sorted_global = sorted(alignments, key=lambda x: x[2], reverse=True)
+sorted_local = sorted(alignments, key=lambda x: x[3], reverse=True)
+
+print('> Top 5 global alignments on protein_sequences.fas')
+for line in sorted_global[:5]:
+    print('    > %s %s - %d' % (line[0], line[1], line[2]))
+
+print('> Top 5 local alignments on protein_sequences.fas')
+for line in sorted_local[:5]:
+    print('    > %s %s - %d' % (line[0], line[1], line[3]))
 press_enter()
 
-print('Creating and \'pretty printing\' sequence...')
-dna = bioseq.DNASeq(valid_dna)
-dna.pretty_print()
+seq1 = 'GATTACA'
+seq2 = 'GCATGCT'
+print('> C2')
+print('We\'ll now be calculating local and global alignments for %s and %s, with match = 1, mismatch = -1, and gap = -1' % (seq1, seq2))
 press_enter()
 
-print('Computing symbol frequency...')
-for k,v in dna.symbol_frequency().items():
-    print('    ' + k + ' : ' + str(v) + ' occurences')
+print('First, global alignment...')
+press_enter()
+sm = bioseq.alignment.create_substitution_matrix('ATCG', 1, -1)
+al = bioseq.alignment(sm)
+al.run_global_align_multiple_solutions(seq1, seq2, -1, debug=True)
 press_enter()
 
-print('Computing GC-content...')
-print('    GC-content corresponds to %.2f%% of the sequence' % (dna.gc_content()*100))
+print('Now, local alignment...')
+press_enter()
+al.run_local_align_multiple_solutions(seq1, seq2, -1, debug=True)
 press_enter()
 
-print('Computing transcription...')
-print('The result is an instance of RNASeq.')
-rna = dna.transcription()
-rna.pretty_print()
+print('> D')
+print('We\'ll now be showcasing compare_pairwise_global_align with a few sample sequences')
+press_enter()
+al = bioseq.alignment()
+seqs = ['AGT', 'ACG','TAG', 'CCT']
+al.compare_pairwise_global_align(seqs, -3)
 press_enter()
 
-print('Computing translation...')
-print()
-print('The result is the same if we start with the original DNA sequence or the result of the previous transcription execution')
-print('This returns an instance of AminoacidSeq')
-print()
-aa1 = dna.translation()
-aa2 = rna.translation()
-
-print('Result of translating based on DNA sequence:') 
-print('    ' + str(aa1))
-print('Result of translating based on RNA sequence:')
-print('    ' + str(aa2))
+print('We\'ll now be showcasing compare_pairwise_local_align with a few sample sequences')
 press_enter()
-
-print('Computing all possible proteins of the resulting aminoacid sequence...')
-for i in aa1.all_proteins_rf():
-    print('    ' + str(i))
-press_enter()
-
-print('Computing the DNA\'s sequence codon usage of \'A\'...')
-for k,v in dna.codon_usage('A').items():
-    print('    ' + k + ' : ' + str(v) + ' occurences')
-press_enter()
-
-print('Computing the DNA sequence\'s reading frames...')
-for i in dna.reading_frames():
-    print('    ' + str(i))
-press_enter()
-
-print('Computing all putative proteins of an DNA or RNA sequence. (all open reading frames)')
-for i in dna.all_open_reading_frames():
-    print('    ' + str(i))
-press_enter()
-
-print('The same as above, except this time with a minimum sequence length of 6')
-orf = dna.all_open_reading_frames(6)
-for i in orf:
-    print('    ' + str(i))
-press_enter()
-
-print('Saving the previous open reading frames (ProteinSeqs) to separates files...')
-for i,o in enumerate(orf):
-    o.save_to_file('protein' + str(i) + '.txt')
-
-print('The files\' content can be later loaded using:')
-print('    bioseq.DNASeq.load_from_file(filename)')
-press_enter()
-
-print('Loading a different DNA sequence, this time from a FASTA file...')
-print()
-print('The result is a dictionary mapping sequence description to an instance of a subclass of BioSeq.')
-fasta_dic = bioseq.read_fasta_file('examples/dna.txt') 
-for k,v in fasta_dic.items():
-    print(k)
-    v.pretty_print()
+al = bioseq.alignment()
+seqs = ['AGT', 'ACG','TAG', 'CCT']
+al.compare_pairwise_local_align(seqs, -3)
 press_enter()
