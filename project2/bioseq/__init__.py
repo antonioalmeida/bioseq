0bc94f8e627665f248098162228afcefbc69fa3f
antonioalmeida <theantonioalmeida@gmail.com>
2019-07-31 01:58:30 +0100

    start repo cleanup

--- project2/bioseq/__init__.py
+++
@@ -1,7 +0,0 @@
-from bioseq.bioseq import BioSeq, SequenceType
-from bioseq.dnracommon import DRNACommon
-from bioseq.dnaseq import DNASeq
-from bioseq.rnaseq import RNASeq
-from bioseq.proteinseq import AminoacidSeq, ProteinSeq
-from bioseq.fasta import read_fasta_file
-from bioseq.alignment import Alignment as alignment
